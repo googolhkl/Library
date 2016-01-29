13b4ddf277595c366e460e85a74474ec9d07abe5
KyeongHoon Lee <googolhkl@gmail.com>
2016-01-29 18:23:14 +0900

    makefile과 공유라이브러리 추가

--- Util/Util.h
+++ Util/Util.h
@@ -1,4 +1,19 @@
 #ifndef __UTIL_H__
 #define __UTIL_H__
+#include <random>
+
+using namespace std;
+
+namespace hkl{
+
+        class rand{
+            private:
+            public:
+            rand(){};
+            ~rand(){};
+            static long long Generate();                                            //임의범위 난수 하나 발생
+            static long long Generate(const long long start,const long long end);   //지정범위 난수 하나 발생
+        };
+}
 
 #endif
