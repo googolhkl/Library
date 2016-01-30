6708dca4c86e5b98c1dc3530e334137dd8c30ba7
KyeongHoon Lee <googolhkl@gmail.com>
2016-01-30 12:06:16 +0900

    NumofSuccess까지 완성

--- Util/Util.h
+++ Util/Util.h
@@ -14,13 +14,12 @@ namespace hkl{
             ~rand(){};
             static long long MakeRangeNum();                                            //임의범위 난수 하나 발생
             static long long MakeRangeNum(const long long start,const long long end);   //지정범위 난수 하나 발생
-            //todo
-            static bool TrueOrFalse(const int       probability);                       // n%확률로 true 또는 or생성
-            static bool TrueOrFalse(const double    probability);   
-            
-            static long long NumOfSuccess(const long long obj, const int    probability);// 성공횟수 확률
+            static bool TrueOrFalse(int             probability);                       // n%확률로 true 또는 or생성
+            static bool TrueOrFalse(const double    probability);
+            static long long NumOfSuccess(const long long obj, const int    probability) throw(int);// 성공횟수 확률
             static long long NumOfSuccess(const long long obj, const double probability);
-
+            
+            //todo
             static long double MakeNearByNum(long long obj,long long deviation);        //표준편차
         };
 }
--- Util/main.cpp
+++ Util/main.cpp
@@ -5,12 +5,11 @@
 using namespace std;
 
 int main(){
-    cout<<"hello world"<<endl;
-    cout<< hkl::rand::MakeRangeNum(1,10)<<endl;
     return UnitTest::RunAllTests();
 }
 
 SUITE(totaltest){
+    // MakeRangeNum테스트
     TEST(test1){
         if(hkl::rand::MakeRangeNum(1,10) ){
             CHECK(true);
@@ -19,12 +18,43 @@ SUITE(totaltest){
         }
     }
 
+    // TrueOrFalse테스트
+    // 100%확률로 통과
     TEST(test2){
-        if(hkl::rand::MakeRangeNum(1,10) ){
+        if(hkl::rand::TrueOrFalse(100) ){
             CHECK(true);
         } else{
             CHECK(false);
         }
     }
+    // 100%확률로 통과
+    TEST ( test3 ){
+        if(hkl::rand::TrueOrFalse(1.0) ){
+            CHECK(true);
+        } else{
+            CHECK(false);
+        }
+    }
+    
+    // NumOfSuccess테스트
+    TEST ( numofsuccess ){
+        int num = hkl::rand::NumOfSuccess(1000,0.45);
+        if(num){
+            cout<<"num: "<<num<<endl;
+            CHECK(true);
+        } else{
+            CHECK(false);
+        }
+    }
+    TEST ( numofsuccess2 ){
+        int num = hkl::rand::NumOfSuccess(1000,45);
+        if(num){
+            cout<<"num2: "<<num<<endl;
+            CHECK(true);
+        } else{
+            CHECK(false);
+        }
+    }
+    // MakeNearByNum테스트
 }
 
